0ff19ece5a2acfa2a95bd919a90a683545aa758e
SUSHANTA NIGUDKAR <nsushant683@gmail.com>
2025-11-14 08:58:03 +0100

    animation script fix

--- src/LowThrustAnalytical.cpp
+++ src/LowThrustAnalytical.cpp
@@ -1,35 +1,27 @@
-#include <iostream>
-#include <iomanip>
-#include <cmath>
-#include <vector>
-#include <array>
-#include <string>
-#include <fstream> 
-#include <armadillo>
-#include <algorithm>
 #include "Nbody.hpp"
+#include <algorithm>
+#include <armadillo>
+#include <array>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
 
+double calculate_edelbaum_deltaV(double v0, double v1,
+                                 double plane_diff_angle) {
 
+  // assumes the transfer is between two circular orbits
+  // assumes that there is no RAAN difference
 
+  double deltaV =
+      std::sqrt(pow(v0, 2) + pow(v1, 2) -
+                2 * v1 * v0 * std::cos(M_PI / 2 * plane_diff_angle));
 
-
-
-
-double calculate_edelbaum_deltaV(double v0,double v1, double plane_diff_angle){
-    
-    // assumes the transfer is between two circular orbits
-    // assumes that there is no RAAN difference
-    
-    double deltaV = std::sqrt(pow(v0,2) + pow(v1,2) - 2 * v1 * v0 * std::cos(M_PI/2 * plane_diff_angle) );
-
-    return deltaV;
-
+  return deltaV;
 }
 
+void low_thrust_di_carlo_independent() {};
 
-void low_thrust_di_carlo_independent(){};
-
-
-void low_thrust_di_carlo_twophase(){};
-
-
+void low_thrust_di_carlo_twophase() {};
